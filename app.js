256c684261449cf3a97ae628bb8e0ee41e87540e
BoognishSaves <johnpaulhaddad@hotmail.com>
2022-08-09 08:26:06 -0600

    HTML & CSS progress...Now to JS

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-// console.log('Hello')
-
 // Psuedocode
 
 // Selecting first card starts game 
@@ -11,3 +9,24 @@
 // Game finishes after last match is made 
 // How to shuffle? (can I userandom number generator?)
 // Will add sounds when there is match and on game end
+
+
+// 3D click  n flip
+const cards = document.querySelectorAll('.memory-card');
+console.log(cards)
+
+function flipCard() {
+   if (this.className === "memory-card"){
+    this.className=("memory-card_flip");
+   //  call firstCard function here? firstCard()
+   } else if(this.className === "memory-card_flip") {
+    this.className=("memory-card");
+   }
+}
+cards.forEach(card => card.addEventListener('click', flipCard));
+
+// Set first card
+
+// function firstCard() {
+//     if (this.)
+// }
