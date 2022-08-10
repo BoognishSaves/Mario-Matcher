2a1761466c03e1b2ef959327ed49de7d71174f84
BoognishSaves <johnpaulhaddad@hotmail.com>
2022-08-10 07:08:13 -0600

    We have a timer

--- app.js
+++ app.js
@@ -12,21 +12,80 @@
 
 
 // 3D click  n flip
+let countClicks = 0;
 const cards = document.querySelectorAll('.memory-card');
-console.log(cards)
+const timerDisplay = document.querySelector('.stop-watch');
+let seconds = 0;
+let interval  = null;
 
 function flipCard() {
+   if (countClicks === 0) {
+    timer();
+    startTimer()
+    playSoundGo()
+   }
    if (this.className === "memory-card"){
     this.className=("memory-card_flip");
+    countClicks +=1
+   
    //  call firstCard function here? firstCard()
-   } else if(this.className === "memory-card_flip") {
-    this.className=("memory-card");
+//    } else if(this.className === "memory-card_flip") {
+//     this.className=("memory-card");
    }
 }
 cards.forEach(card => card.addEventListener('click', flipCard));
 
-// Set first card
+// Matching
 
-// function firstCard() {
-//     if (this.)
+// const doesItMatch = document.querySelectorAll('.memory-card_flip');
+// console.log(doesItMatch)
+// doesItMatch.forEach()
+
+
+
+
+// Timer
+
+function timer() {
+    seconds++;
+
+    let hrs = Math.floor(seconds / 3600);
+    let mins = Math.floor((seconds - (hrs * 3600)) / 60);
+    let secs = seconds % 60;
+    if (secs < 10) secs = '0' + secs;
+    if (mins< 10) mins = '0' + mins;
+    
+    timerDisplay.innerText = `${mins}:${secs}`;
+}
+
+function startTimer() {
+    if(interval) {
+        return
+    }
+    interval = setInterval(timer, 1000);
+}
+
+function stopTimer() {
+    clearInterval(interval);
+    interval = null;
+}
+
+function playSoundGo() {
+    const congrats = document.getElementById("letsaGo");
+    letsaGo.play();
+}
+
+
+
+// Game Ends
+
+// function endOfGame() {
+//     stopTimer();
+//     seconds = 0;
+//     timerDisplay.innerText = '00:00';
 // }
+
+
+ // console.log(cards[9].id);
+
+// cards.addEventListener('click', clicked)
