72e9e8763389a40f9886bb37b99775a73e5ecc23
BoognishSaves <johnpaulhaddad@hotmail.com>
2022-08-11 06:03:51 -0600

    Still trying to get over some hurdles

--- app.js
+++ app.js
@@ -14,32 +14,85 @@
 // 3D click  n flip
 let countClicks = 0;
 const cards = document.querySelectorAll('.memory-card');
+// const singleCard = document.querySelectorAll('*[id]');
+// console.log(singleCard)
 const timerDisplay = document.querySelector('.stop-watch');
 let seconds = 0;
 let interval  = null;
+let firstCard, secondCard
 
 function flipCard() {
+    let match  = false;
    if (countClicks === 0) {
     timer();
     startTimer()
     playSoundGo()
    }
+
    if (this.className === "memory-card"){
     this.className=("memory-card_flip");
     countClicks +=1
-   
-   //  call firstCard function here? firstCard()
-//    } else if(this.className === "memory-card_flip") {
-//     this.className=("memory-card");
-   }
 }
-cards.forEach(card => card.addEventListener('click', flipCard));
+//  below will allow cards to be fliped back over. Use just for testing 
+// else if(this.className === "memory-card_flip") {
+// this.className=("memory-card");}
 
-// Matching
+   if(countClicks % 2 == 0  && countClicks != 0) {
+    // matchfunction 
+    secondCard = this.id;
+    console.log(secondCard)
+    
+   } else {
+    firstCard = this.id;
+    console.log(firstCard)
+   }
 
-// const doesItMatch = document.querySelectorAll('.memory-card_flip');
-// console.log(doesItMatch)
-// doesItMatch.forEach()
+   if (firstCard === secondCard && countClicks <3 && countClicks >1) {
+//  match!!
+    match = true;
+     console.log('match')
+    firstCard= '';
+    secondCard= '';
+    countClicks= 0;
+   } 
+  else if (firstCard !== secondCard && countClicks <3 && countClicks >1) {
+    console.log('Not a  match')
+    firstCard= '';
+    secondCard= '';
+    countClicks= 0;
+    
+  }
+//  console.log(countClicks)
+//  console.log(firstCard)
+//  console.log(secondCard)
+//  console.log(match)
+}
+cards.forEach(card => card.addEventListener('click', flipCard, {once: true}));
+
+// Shuffle
+// Found online. Not working 
+// function shuffle() {
+//     var container = document.getElementsByClassName("memory-card");
+//     var elementsArray = Array.prototype.slice.call(container.getElementsByClassName('memory-card'));
+//       elementsArray.forEach(function(element){
+//       container.removeChild(element);
+//     })
+//     shuffleArray(elementsArray);
+//     elementsArray.forEach(function(element){
+//     container.appendChild(element);
+//   })
+//   }
+  
+//   function shuffleArray(array) {
+//       for (var i = array.length - 1; i > 0; i--) {
+//           var j = Math.floor(Math.random() * (i + 1));
+//           var temp = array[i];
+//           array[i] = array[j];
+//           array[j] = temp;
+//       }
+//       return array;
+//   }
+
 
 
 
@@ -74,18 +127,29 @@ function playSoundGo() {
     const congrats = document.getElementById("letsaGo");
     letsaGo.play();
 }
+function playSoundCongrats() {
+    const congrats = document.getElementById("congrats");
+    congrats.play();
+}
 
 
 
 // Game Ends
 
+// function youWin() {
+//     if (document.querySelectorAll(".memory-card_flip").length === 8) {
+//         endfGame()
+//         playSoundCongrats()
+//     }
+// }
+
 // function endOfGame() {
+    // push value  to ul before  clearing 
 //     stopTimer();
 //     seconds = 0;
 //     timerDisplay.innerText = '00:00';
 // }
 
 
- // console.log(cards[9].id);
+//  console.log(cards[9].id);
 
-// cards.addEventListener('click', clicked)
